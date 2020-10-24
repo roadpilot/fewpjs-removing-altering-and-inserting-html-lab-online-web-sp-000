e201221b00d722c59ba538b48219de978533deed
Martin Achtermann <mdacht777@gmail.com>
2020-10-24 12:21:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,6 @@
 let header = document.querySelector("main#main");
 header.remove();
 
-let newHeader = document.createElement('h1#victory');
+let newHeader = document.createElement('h1');
+let x = newHeader.createElement('victory')
 document.appendChild(newHeader);
