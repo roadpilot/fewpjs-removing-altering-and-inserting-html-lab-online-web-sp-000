2976b218aec741eeaea47835fc2c3a43d05efdbd
Martin Achtermann <mdacht777@gmail.com>
2020-10-24 12:30:53 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+let header = document.querySelector("main#main");
+header.remove();
+
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory'
+newHeader.innerHTML = 'your name is the champion'
+// document.appendChild(newHeader);
